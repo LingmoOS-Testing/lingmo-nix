d5e122b410acd3a79c77047d5d384b9b8b48003d
LainY <114994536+arkimium@users.noreply.github.com>
2024-08-03 18:51:39 +0800

    Update home.nix

--- etc/home-manager/home.nix
+++ etc/home-manager/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "phaethon";
-  home.homeDirectory = "/home/phaethon";
+  home.username = "lingmo";
+  home.homeDirectory = "/home/lingmo";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
